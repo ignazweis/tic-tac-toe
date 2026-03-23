28816a8562d55788a1fdcc7e6bb87ec613340617
ignazweis2 <213210679+ignazweis2@users.noreply.github.com>
2026-03-23 14:48:29 -0300

    refactor: great change in the code to performance and simplify
    - change the caractere player in the code with "#define"
    - change the caractere space in the table with "#define"
    - calc function dont need player variable
    - use ternary operator when needed
    - "else if" when see the win, tie or continue game
    - don't need "for" to search to change the table
    - add "ctype.h" library
    - and others

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int calc(char p[], char player, int move);
+#define P1 'X'
+#define P2 'O'
+#define DOT '.'
+#define DOTS {DOT, DOT, DOT, DOT, DOT, DOT, DOT, DOT, DOT}
+
+int calc(char p[], int move);
 char change_player(char player);
 
 int main (void)
 {
-    char p[9], player;
-    bool win = false, invalid_move;
+    char p[9] = DOTS, player;
+    bool win = false;
     int move = 0, buff;
-    
-    for (int i = 0; i < 9; i++)
-    {
-        p[i] = '.';
-    }
 
-    printf("Which player goes first? [O/X]\n");
+    printf("Which player goes first? [%c/%c]\n", P2, P1);
     printf("> ");
     scanf("%1c", &player);
     getchar();
     printf("\n");
 
-    if (player == 111 || player == 120)
-    {
-        player = player - 32;
-    }
+    player = toupper(player);
 
-    if (player != 'X' && player != 'O')
+    if (player != P1 && player != P2)
     {
         printf("Invalid player.\n\n");
         return 1;
@@ -45,8 +43,7 @@ int main (void)
         {
             printf("[%c] player\n", player);
         }
-
-        if (win == true)
+        else if (win)
         {
             player = change_player(player);
 
@@ -62,7 +59,6 @@ int main (void)
         }
 
         printf("> ");
-
         scanf("%1i", &buff);
         getchar();
         printf("\n");
@@ -71,46 +67,34 @@ int main (void)
         if (buff >= 1 && buff <= 9 && p[pos] == '.')
         {
             p[pos] = player;
-            invalid_move = false;
+            win = calc(p, move);
+            player = change_player(player);
+            move++;
         }
         else
         {
-            invalid_move = true;
-        }
-
-        if (invalid_move == true)
-        {
             printf("\n");
-            printf("Jogada inválida.");
+            printf("Invalid play.");
             printf("\n\n");
-        }
-
-        win = calc(p, player, move);
-
-        player = change_player(player);
-
-        if (invalid_move == false)
-        {
-            move++;
         }
     }
     return 0;
 }
 
-int calc(char p[], char player, int move)
+int calc(char p[], int move)
 {
     if (move >= 4)
     {
-        if ((p[0] == player && p[1] == player && p[2] == player) ||
-            (p[3] == player && p[4] == player && p[5] == player) ||
-            (p[6] == player && p[7] == player && p[8] == player) ||
+        if ((p[0] != DOT && p[0] == p[1] && p[1] == p[2]) ||
+            (p[3] != DOT && p[3] == p[4] && p[4] == p[5]) ||
+            (p[6] != DOT && p[6] == p[7] && p[7] == p[8]) ||
 
-            (p[0] == player && p[3] == player && p[6] == player) ||
-            (p[1] == player && p[4] == player && p[7] == player) ||
-            (p[2] == player && p[5] == player && p[8] == player) ||
+            (p[0] != DOT && p[0] == p[3] && p[3] == p[6]) ||
+            (p[1] != DOT && p[1] == p[4] && p[4] == p[7]) ||
+            (p[2] != DOT && p[2] == p[5] && p[5] == p[8]) ||
 
-            (p[0] == player && p[4] == player && p[8] == player) ||
-            (p[6] == player && p[4] == player && p[2] == player))
+            (p[0] != DOT && p[0] == p[4] && p[4] == p[8]) ||
+            (p[6] != DOT && p[6] == p[4] && p[4] == p[2]))
             {
                 return 1;
             }
@@ -120,13 +104,5 @@ int calc(char p[], char player, int move)
 
 char change_player(char player)
 {
-    if (player == 'X')
-    {
-        player = 'O';
-    }
-    else
-    {
-        player = 'X';
-    }
-    return player;
+    return (player == P1) ? P2 : P1;
 }
